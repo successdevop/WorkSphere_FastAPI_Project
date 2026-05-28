70e023163fcefea8020a3c81af927f5d3a4a5ffd
successdevop <successraphael28@gmail.com>
2026-05-28 11:32:05 +0100

    implemented simple crud operation to test the functionality of our application

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, status
+from fastapi.exceptions import HTTPException
 from typing import Optional, List, Any
 from pydantic import BaseModel
 
@@ -53,51 +54,50 @@ async def get_all_books() -> dict:
 async def get_a_book(book_id: int) -> Optional[dict]:
     book = next((bk for bk in data if bk["id"] == book_id), None)
     if book is None:
-        return {"error":"book not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
     return book
 
-@app.patch("/book/{book_id}")
+@app.patch("/books/{book_id}")
 async def update_book(book_id: int, book_data: BookModel) -> dict:
     book = next((bk for bk in data if bk["id"] == book_id))
     if not book:
-        return {"error":"book not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
     if "title" in book_data:
-        book_data.title = book["title"]
+        book["title"] = book_data.title
     if "author" in book:
-        book["author"] = book["author"]
+        book["author"] = book_data.author
     if "publisher" in book:
-        book["publisher"] = book["publisher"]
+        book["publisher"] = book_data.publisher
     if "published_date" in book:
-        book["published_date"] = book["published_date"]
+        book["published_date"] = book_data.published_date
     if "page_count" in book:
-        book["page_count"] = book["page_count"]
+        book["page_count"] = book_data.page_count
     if "language" in book:
-        book["language"] = book["language"]
+        book["language"] = book_data.language
 
     return book
 
-@app.put("/book/{book_id}")
-async def update_book_put(book_id: int, book_data: BookModel) -> dict:
-    book = next((bk for bk in data if bk["id"] == book_id))
-    if not book:
-        return {"error":"book not found"}
+@app.put("/books/{book_id}")
+async def update_book_put(book_id: int, book_data: BookModel):
+    book = next((bk for bk in data if bk["id"] == book_id), None)
+    if book:
+        book["title"] = book_data.title
+        book["publisher"] = book_data.publisher
+        book["page_count"] = book_data.page_count
+        book["language"] = book_data.language
+        return book
 
-    book = {"id": book_id,
-            "author": book_data.author,
-            "publisher": book_data.publisher,
-            "published_date": book_data.published_date,
-            "page_count": book_data.page_count,
-            "language": book_data.language
-            }
-    return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 @app.delete("/books/{book_id}")
-async def update_book_put(book_id: int) -> dict:
+async def update_book_put(book_id: int):
     book = next((bk for bk in data if bk["id"] == book_id), None)
-    if not book:
-        return {"error":"book not found"}
-    return data.pop(book_id)
+    if book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    
+    data.remove(book)
+    return {"message":"Deleted", "status":status.HTTP_200_OK}
 
 @app.post("/books")
 async def add_book(book_data: BookModel):
@@ -110,6 +110,12 @@ async def add_book(book_data: BookModel):
             "language": book_data.language
             }
     if any(bk["id"] == book["id"] for bk in data):
-        return {"error": "book id already exist. Assign another ID"}
+        return {"error": "book id already exist. Assign another ID", "status":status.HTTP_400_BAD_REQUEST}
+    data.append(book)
+    return book
+
+@app.post("/books2", status_code=status.HTTP_201_CREATED)
+async def create_book(book_data: BookModel):
+    book = book_data.model_dump()
     data.append(book)
     return book
